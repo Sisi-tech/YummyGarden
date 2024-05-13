d568a0b925a88e28a78a6a7440332c6d59979f6e
Sisi Wang <sisiwang242@gmail.com>
2024-05-12 23:09:49 -0400

    add min and max number to the guest count input

--- myapp/forms.py
+++ myapp/forms.py
@@ -1,11 +1,18 @@
 from django import forms
 from .models import Booking
 from .models import Menu 
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 class BookingForm(forms.ModelForm):
     class Meta:
         model = Booking 
         fields = '__all__'
+        widgets = {
+            'guest_count': forms.NumberInput(attrs={'min': 1, 'max': 12}),
+        }
+        validators = {
+            'guest_count': [MinValueValidator(1), MaxValueValidator(12)],
+        }
 
 class MenuItems(forms.ModelForm):
     class Meta:
--- myapp/models.py
+++ myapp/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 # Create your models here.
 class DrinksCategory(models.Model):
@@ -14,9 +15,9 @@ class Drinks(models.Model):
 class Booking(models.Model):
     first_name = models.CharField(max_length=200)
     last_name = models.CharField(max_length=200)
-    guest_count = models.IntegerField()
-    reservation_time = models.DateField(auto_now=True)
-    comments = models.CharField(max_length=1000)
+    guest_count = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(12)])
+    reservation_time = models.DateTimeField(auto_now=True)
+    comments = models.CharField(max_length=1000, null=True)
 
     def __str__(self):
         return self.first_name + " " + self.last_name
